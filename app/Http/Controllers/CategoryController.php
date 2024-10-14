ec691068fbad12f08db0e7587564b4e2cf834b20
ibrokxim <nibrohim04@gmail.com>
2024-10-14 10:18:51 +0500

    optimization category responce with cache

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,21 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Cache;
 
 class CategoryController extends Controller
 {
     public function getAllCategories()
     {
-        $tree = [];
-        $categories = Category::whereNull('parent_id')
-            ->with(['subcategories' => function ($query) {
-                $query->with('subcategories'); // Загружаем подкатегории первого уровня
-            }])
-            ->chunk(100, function ($categories) use (&$tree) {
-                foreach ($categories as $category) {
-                    $tree[] = $this->buildCategoryTree($category);
-                }
-            });
+        $tree = Cache::remember('all_categories', 60, function () {
+            $tree = [];
+            $categories = Category::whereNull('parent_id')->lazy();
+
+            foreach ($categories as $category) {
+                $tree[] = $this->buildCategoryTree($category);
+            }
+
+            return $tree;
+        });
 
         return response()->json($tree);
     }
@@ -27,19 +28,22 @@ class CategoryController extends Controller
     {
         $tree = [
             'id' => $category->id,
-            'parent_id' => $category->parent_id,
-            'image' => $category->picture !== null ? ENV('APP_URL') . $category->picture : null,
             'name' => $category->name,
             'slug' => $category->slug,
+            'parent_id' => $category->parent_id,
+            'image' => $category->picture !== null ? ENV('APP_URL') . $category->picture : null,
             'subcategories' => []
         ];
 
-        foreach ($category->subcategories as $subcategory) {
+        $subcategories = $category->subcategories()->with('subcategories')->lazy();
+
+        foreach ($subcategories as $subcategory) {
             $tree['subcategories'][] = $this->buildCategoryTree($subcategory);
         }
 
         return $tree;
     }
+
 
     public function showCategoryBySlug($slug)
     {
